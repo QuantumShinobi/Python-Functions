ed7678b768e7a1de83efa9809f47aac43a3117b8
IamEinstein1 <vermarishit039@gmail.com>
2020-09-09 16:09:32 +0530

    commit

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ class SquareTestCase(unittest.TestCase):
     def test_area(self):
         square = Square(10)
         area = 100
-        self.assertEqual(area, square.area)
+        self.assertEqual(area, square.area())
 
     def test_perimeter(self):
         square = Square(10)
         perimeter = 40
-        self.assertEqual(perimeter, square.perimeter)
+        self.assertEqual(perimeter, square.perimeter())
