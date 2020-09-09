5384f971485a05d6f9e8f85049727283857564d8
IamEinstein1 <vermarishit039@gmail.com>
2020-09-09 16:18:55 +0530

    rectangle

--- test.py
+++ test.py
@@ -16,5 +16,18 @@ class SquareTestCase(unittest.TestCase):
         self.assertEqual(perimeter, square.perimeter())
 
 
+class RectangleTestCase(unittest.TestCase):
+
+    def test_area(self):
+        rectangle = Rectangle(10, 20)
+        area = 200
+        self.assertEqual(area, Rectangle.area())
+
+    def test_perimeter(self):
+        rectangle = Rectangle(10, 20)
+        perimeter = 60
+        self.assertEqual(perimeter, Rectangle.perimeter())
+
+
 if __name__ == "__main__":
     unittest.main()
