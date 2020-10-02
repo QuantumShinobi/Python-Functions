d09505c74ec251583893495189a55ed367abd599
IamEinstein1 <vermarishit039@gmail.com>
2020-10-02 08:52:16 +0530

    added github pages

--- math.py
+++
@@ -1,4 +0,0 @@
-def sum():
-    num1 = float(input('What is the 1st num\n'))
-    num2 = float(input('What is the 2nd num\n'))
-    print(num1+num2)
--- root_calc.py
+++
@@ -1,9 +0,0 @@
-def func():
-    num = int(input('What is the number\n'))
-    root = int(input('What is the power\n'))
-    if root != 0 and root > 0:
-        ans = num**(1/root)
-        print(ans)
-        return ans
-    else:
-        print("INVALID!!!!!!!🐙 ")
